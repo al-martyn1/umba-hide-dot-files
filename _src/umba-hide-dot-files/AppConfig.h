4cf4d06ef416cfd8402c020ca886ac1b0a3397b4
al-martyn1 <amart@mail.ru>
2024-12-26 02:38:17 +0300

    Накидываю дальше

--- _src/umba-hide-dot-files/AppConfig.h
+++ _src/umba-hide-dot-files/AppConfig.h
@@ -11,13 +11,64 @@
 #include <map>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
+
+struct CommandInfo
+{
+    Command       cmd     = Command::none;
+    bool          recurse = false;
+    std::string   path;
+
+};
 
 struct AppConfig
 {
-    Command       cmd = Command::none;
-    std::string   aliasName;
-    std::string   path;
+    std::vector<CommandInfo>       commands  ;
+    CommandInfo                    curCommand;
+
+    /*
+        Добавляем команду в очередь только при получении новой команды,
+        или после окончания обработки аргументов командной строки, если текущая команда не Command::none.
+
+        При добавлении команды всё обнуляем в текущей, ставим cmd = Command::none
+     */
+
+
+    bool startNewCommand(Command c)
+    {
+        // Проверку валидности команд сделаем перед обработкой
+        commands.emplace_back(curCommand);
+        curCommand = CommandInfo{};
+    }
+
+    bool finalizeCommands()
+    {
+        if (curCommand.cmd==Command::none)
+        {
+            return curCommand.path.empty();
+        }
+
+        commands.emplace_back(curCommand);
+        curCommand = CommandInfo{};
+        return true;
+    }
+
+    bool setPath(const std::string &p)
+    {
+        if (curCommand.cmd==Command::none)
+            return false;
+
+        if (!curCommand.path.empty())
+            return false;
+
+        curCommand.path = p;
+
+        return true;
+    }
+
 
 }; // struct AppConfig
+
+
 
